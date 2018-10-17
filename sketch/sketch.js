0ce69c6ea4fadad3b48e89a37ecd57ec2c157e1e
Daniel Kapica <kapica6105@gmail.com>
2018-10-17 11:45:26 +0100

    Update sketch/sketch.js
    Made custom functions to create triangle and square, then used those to create a custom function which draws a house.

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -6,18 +6,20 @@ function setup() {
   function draw() {
     fill(255, 130, 0)
     stroke(0)
-    triangle(0, 30, 40, 0, 80, 30)
-    square(5, 30, 70)
+    house(5, 30, 70, 0, 30, 40, 0, 80, 30)
   }
 
   function square(x, y, size) {
     rect(x, y, size, size)
   }
 
-  function roof(x, y) {
+  function roof(x1, y1, x2, y2, x3, y3) {
     // ...
+    triangle(x1, y1, x2, y2, x3, y3)
   }
   
-  function house(x, y) {
+  function house(x, y, size, x1, y1, x2, y2, x3, y3) {
     // ...
+    square(x, y, size, size)
+    roof(x1, y1, x2, y2, x3, y3)
   }
