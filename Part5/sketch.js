124f6a6ac1a69f388122bff7d6a74e61cf861473
Daniel Kapica <kapica6105@gmail.com>
2018-10-18 11:59:58 +0100

    Update Part5/sketch.js

--- Part5/sketch.js
+++ Part5/sketch.js
@@ -1,48 +1,49 @@
 var x = 0
 var speed = 3
-var userY = 30
+var paddleY = 30
 var isPaddleColldingTop = false
 var isPaddleColldingBottom = false
 var screenWidth = 300
 var screenHeight = 100
+var isBallCollidingPaddle = false
 
 function setup() {
   createCanvas(screenWidth, screenHeight)
 }
 
 function draw() {
-  background(0)
-
-  x = x + speed
-
-  if (x < 0) {
-    speed = speed * -1
-  } else if (x > 300) {
-    speed = speed * -1
-  }
-  
+  background(0)  
   ellipse(x, 30, 10, 10)
-  rect(0,userY,5,20)
-
+  rect(0,paddleY,5,20)
   keyPressed()
   paddleColliding()
 }
 
 function keyPressed() {
     if (key == 'w' && !isPaddleColldingTop) {
-      userY = userY - 2
+      paddleY = paddleY - 2
     } else if (key == 's' && !isPaddleColldingBottom){
-        userY = userY + 2
+        paddleY = paddleY + 2
     }
 }
 
 function paddleColliding(){
-    if (userY >= (screenHeight-20)) {
+    if (paddleY >= (screenHeight-20)) {
         isPaddleColldingBottom = true
-    } else if(userY <= 0){
+    } else if(paddleY <= 0){
         isPaddleColldingTop = true
-    }else{
+    } else{
         isPaddleColldingBottom = false
         isPaddleColldingTop = false
+    }
+}
+
+function ballColliding(){
+    x = x + speed
+    if (x < 0) {
+      speed = speed * -1
+
+    } else if (x > 300) {
+      speed = speed * -1
     }
 }
