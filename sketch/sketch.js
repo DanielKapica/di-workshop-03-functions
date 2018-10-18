b9b7a8380d385ea4b6c40171f530adcd414e7681
Daniel Kapica <kapica6105@gmail.com>
2018-10-18 10:34:12 +0100

    Created 3 houses

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -4,9 +4,11 @@ function setup() {
   }
   
   function draw() {
-    fill(blue)
+    fill("blue")
     stroke(0)
-    house(5, 50, 80, 0, 50, 50, 0, 80, 50)
+    house(0, 50, 100, 0, 50, 50, 0, 100, 50)
+    house(100, 150, 100, 100, 150, 150, 100, 200, 150)
+    house(200, 250, 100, 200, 250, 250, 200, 300, 250)
   }
 
   function square(x, y, size) {
@@ -14,12 +16,10 @@ function setup() {
   }
 
   function roof(x1, y1, x2, y2, x3, y3) {
-    // ...
     triangle(x1, y1, x2, y2, x3, y3)
   }
   
   function house(x, y, size, x1, y1, x2, y2, x3, y3) {
-    // ...
-    square(x, y, size, size)
+    square(x, y, size)
     roof(x1, y1, x2, y2, x3, y3)
   }
