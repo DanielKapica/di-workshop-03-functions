51fbdf0c498f59dd5e8b21d939db0e192d195dcc
Daniel Kapica <kapica6105@gmail.com>
2018-10-19 15:20:10 +0100

    Refactored the code

--- Part5/sketch.js
+++ Part5/sketch.js
@@ -1,3 +1,5 @@
+// Created by Daniel Kapica on 17/10/2018
+
 var ballX = 15
 var speed = 3
 var paddleY = 40
@@ -12,49 +14,53 @@ var score = 0
 var paddleHeight = 20
 
 function setup() {
-  createCanvas(screenWidth, screenHeight)
+    createCanvas(screenWidth, screenHeight)
 }
 
 function draw() {
-  background(100)
-  if (!userLost){
-    if((keyIsDown(UP_ARROW)) && !isPaddleColldingTop){
-        paddleY = paddleY - 2
-      } else if (((keyIsDown(DOWN_ARROW)) && !isPaddleColldingBottom)){
-          paddleY = paddleY + 2
-      }
-    ellipse(ballX, ballY, 10, 10)
-    rect(0,paddleY,5,paddleHeight)
-    paddleColliding()
-    ballColliding()
-    text("Score: ", 220, 10)
-    text(score, 290, 10)
-  }else{
-      text("YOU LOST! Your score was: ",10,50)
-      text(score,50,75)
-  }
+    background(100)
+    if (!userLost) {
+        if ((keyIsDown(UP_ARROW)) && !isPaddleColldingTop) {
+            paddleY = paddleY - 2
+        } else if (((keyIsDown(DOWN_ARROW)) && !isPaddleColldingBottom)) {
+            paddleY = paddleY + 2
+        }
+        ellipse(ballX, ballY, 10, 10)
+        rect(0, paddleY, 5, paddleHeight)
+        paddleColliding()
+        ballColliding()
+        text("Score: ", 220, 10)
+        text(score, 290, 10)
+    } else {
+        text("YOU LOST! Your score was: ", 10, 50)
+        text(score, 50, 75)
+    }
 }
 
-function paddleColliding(){
-    if (paddleY >= (screenHeight-paddleHeight)) {
+function paddleColliding() {
+    if (paddleY >= (screenHeight - paddleHeight)) {
         isPaddleColldingBottom = true
-    } else if(paddleY <= 0){
+    } else if (paddleY <= 0) {
         isPaddleColldingTop = true
-    } else{
+    } else {
         isPaddleColldingBottom = false
         isPaddleColldingTop = false
     }
 }
 
-function ballColliding(){
+function ballColliding() {
     ballX = ballX + speed
-    if ((ballY > paddleY && ballY < paddleY+paddleHeight && ballX<10)) {
-      speed = speed * -1
+    if ((ballY > paddleY && ballY < paddleY + paddleHeight && ballX < 10)) {
+        speed = speed * -1
     } else if (ballX > 300) {
-      speed = speed * -1
-      score++
-    } else if (ballX < 0){
+        speed = speed * -1
+        score++
+    } else if (ballX < 0) {
         speed = 0
         userLost = true
     }
+}
+
+function randomDirectionGenerator() {
+    // Change the Y axis of the ball
 }
